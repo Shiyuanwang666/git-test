61f4dff14c79315a52b4d3f298a6af02a7de59c6
wsy <707376944@qq.com>
2021-10-28 10:14:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wsy
+ * @create 2021-10-28 10:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
